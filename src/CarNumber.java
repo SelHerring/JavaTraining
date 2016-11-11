5fe00f3679eaec73af204862d0dfc2d6bf7cf076
Denis Soldatenkov <d.soldatenkov@parcsis.org>
2016-11-11 04:40:11 +0400

    Добавлен класс генерации номера и класс, унаследованный от него

--- src/CarNumber.java
+++ src/CarNumber.java
@@ -1,25 +1,50 @@
-/**
- * Created by Денис on 09.11.2016.
- */
+import java.util.ArrayList;
+import java.util.Random;
+
 public class CarNumber {
-    public static void main(String[] args) {
-        String letters = "abcdefghijklmnopqrstuvwxyz";
-        String numbers = "0123456789";
-        char[] symbols = new char[2*letters.length() + numbers.length()];
-        int i = 0;
-        for (char letter : letters.toCharArray()) {
-            symbols[i] = letter;
-            i++;
+    private static String TakeLetter() {
+        Random r = new Random();
+        String[] letters = {"А", "В", "Е", "К", "М", "Н", "О", "Р", "С", "Т", "X", "У"};
+
+        return letters[r.nextInt(letters.length)];
+    }
+
+    private static String TakeRegion() {
+        ArrayList<String> regionCodes = new ArrayList(121);
+        ArrayList<String> additionalRegions = new ArrayList(21);
+        Random r = new Random();
+        for (int i = 1; i <= 99; i++) {
+            String s = new String();
+            s += i;
+            if (i <= 9) s = "0" + s;
+            regionCodes.add(s);
+            if (i == 20) regionCodes.remove(s);
         }
-        for (char letter : letters.toUpperCase().toCharArray()) {
-            symbols[i] = letter;
-            i++;
+        for (int i = 0; i <= 22; i++) {
+            String[] s = {"102", "113", "116", "121", "124", "125", "138", "150", "152", "154",
+                    "159", "161", "163", "164", "173", "174", "177", "190", "199", "197", "198", "750", "777"};
+            additionalRegions.add(s[i]);
         }
-        for (char number : numbers.toCharArray()) {
-            symbols[i] = number;
-            i++;
-        }
-        System.out.println(symbols);
+        regionCodes.addAll(additionalRegions);
+        String[] arr = regionCodes.toArray(new String[regionCodes.size()]);
+
+        return arr[r.nextInt(regionCodes.size())];
+    }
+
+    public static String Generation() {
+        Random r = new Random();
+        StringBuilder carNumber = new StringBuilder();
+        carNumber.
+                append(TakeLetter()).
+                append(" ").
+                append(r.nextInt(9)).append(r.nextInt(9)).append(r.nextInt(9)).
+                append(" ").
+                append(TakeLetter()).
+                append(TakeLetter()).
+                append(" ").
+                append(TakeRegion());
+
+        return carNumber.toString();
     }
 }
 
