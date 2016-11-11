5c5fece50c3abed6367a7eba50ae1345fc054fd6
Denis Soldatenkov <d.soldatenkov@parcsis.org>
2016-11-11 04:38:58 +0400

    Добавлен класс генерации номера и класс, унаследованный от него

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+
+
+/**
+ * Created by Денис on 11.11.2016.
+ */
+public class Test extends CarNumber{
+    public static void main(String[] args) {
+        System.out.println(CarNumber.Generation());
+    }
+}
