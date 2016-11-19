3b3b4cd342d1fe1dae9d62261c26cbdb12873b8c
Denis Soldatenkov <d.soldatenkov@parcsis.org>
2016-11-19 04:40:10 +0400

    Генерация СНИЛС

---
+++ src/Second_Chapter/Street.java
@@ -0,0 +1,7 @@
+package Second_Chapter;
+
+/**
+ * Created by Денис on 15.11.2016.
+ */
+public class Street {
+}
---
+++ src/Snils.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Денис on 19.11.2016.
+ */
+public class Snils {
+}
